55cda32503d63dbb6a8e409a9fac93c4813be9e2
arjunbector <arjunbector@gmail.com>
2024-02-22 17:32:20 +0530

    add nav

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex w-full h-10 font-bold p-2 text-xl'>
+      Research<span className='text-blue-500'>Lens</span>
+    </nav>
+  )
+}
+
+export default Navbar
