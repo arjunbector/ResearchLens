7617391197b6812b722e595ffb26ccbb8c3e84cf
arjunbector <arjunbector@gmail.com>
2024-02-23 01:04:23 +0530

    add pdf routes

---
+++ frontend/pages/api/pdf/uploadthing.js
@@ -0,0 +1,25 @@
+import { connectToDB } from "/dbconfig/dbconfig";
+
+
+import {User} from "/models/userModel"
+import { NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+export default async function handler(req, res) {
+    try {
+        await connectToDB();
+        const email=req.body.email;
+        const user=await User.findOne({email:email});
+        
+        const r=user.pdf;
+        console.log(r);
+          console.log(user);
+          
+            res.status(200).json({message:"done"})
+    }catch(error){
+        console.log(error);
+       return res.status(500).json(error);
+    }
+    }
