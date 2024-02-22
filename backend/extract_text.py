ca7ba79353882c231048b07e25210519d3c04655
SamShah30112004 <harshdeepashah@gmail.com>
2024-02-23 05:02:23 +0530

    Some changes to accomodate pull

--- backend/extract_text.py
+++ backend/extract_text.py
@@ -1,4 +1,10 @@
 import fitz
+import base64
+import io
+
+pdf_bytes = base64.b64decode(base64_pdf)
+with open("output.pdf", "wb") as f_out:
+    f_out.write(pdf_bytes)
 
 def extract_text_from_pdf(pdf_path):
     pdf_document = fitz.open(pdf_path)
