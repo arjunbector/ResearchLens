b274fb0b888a819de0b2ed61c668359568b7b323
arjunbector <arjunbector@gmail.com>
2024-02-23 00:41:36 +0530

    add pdf upload functionality

--- frontend/components/Chat.jsx
+++ frontend/components/Chat.jsx
@@ -1,14 +1,48 @@
 "use client";
 
 import { useState } from "react";
+import axios from "axios";
 
 const Chat = () => {
   const [fileUploaded, setFileUploaded] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [progress, setProgress] = useState({
+    started: false,
+    pc: 0,
+  });
 
   const handleUpload = (event) => {
-    setFileUploaded(true);
     console.log(event.target.files[0]);
-    console.log("Uploading file");
+    if (!event.target.files[0]) {
+      console.log("No file selected");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("file", event.target.files[0]);
+    setProgress(prev=>{
+      return {...prev, started: true}
+    
+    })
+    axios
+      .post("/api/pdf/convert", formData, {
+        onUploadProgress: (progressEvent) => {
+          console.log(
+            `Upload Progress: ${Math.round(
+              setProgress(prev=>{
+                return {...prev, pc: (progressEvent.loaded / progressEvent.total) * 100}
+              })
+            )}%`
+          );
+        },
+      },
+      )
+      .then((res) => {
+        console.log(res);
+        setFileUploaded(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <main className="h-[90vh] w-4/5 border-white border-solid border rounded-2xl">
@@ -18,10 +52,14 @@ const Chat = () => {
             className="text-white file:bg-transparent file:border-solid file:border-white file:rounded-xl m-5 file:text-white"
             placeholder="Enter your query"
             type="file"
-            onChange={handleUpload}
+            onChange={(e) => {
+              setSelectedFile(e.target.files[0]);
+              handleUpload(e);
+            }}
           />
         )}
       </div>
+        <p>{progress.pc}</p>
     </main>
   );
 };
